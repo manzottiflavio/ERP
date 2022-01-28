e12aaba87d8a8067822e917e4672427a92e03d05
flavio manzotti <manzottiflavio22@gmail.com>
2022-01-28 17:43:13 -0300

    post product ok

--- src/index.js
+++ src/index.js
@@ -7,21 +7,31 @@ app.listen(5555);
 
 
 const user=[];
+const product=[];
 
 function verifyIfUserAlrearyExists(request,response,next){
 const {cpf}=request.headers;
-
 const users=user.find((users)=>users.cpf===cpf);
-
 if(!users){
-    return response.status(400).json({message:"user alreary exists"});
+    return response.status(400).json({message:"user not found"});
+}
+request.users=users;
+return next();
+};
+
+function verifyIfProductAlrearyExists(request,response,next){
+const {name}=request.headers;
+
+const products=product.find((products)=>products.name===name);
+if(!products){
+    return response.status(400).json({message:"products not found"})
+};
+
+request.products=products;
+return next();
 }
 
-request.users=users;
 
-return next();
-
-};
 
 app.post("/user",(request,response)=>{
 const {name,cpf}=request.body;
@@ -29,7 +39,7 @@ const {name,cpf}=request.body;
 const users=user.some((users)=>users.cpf===cpf);
 
 if(users){
-    return response.status(400).json({message:"user not found"});
+    return response.status(400).json({message:"user alreary exists"});
 }
 
 user.push({
@@ -51,3 +61,27 @@ app.get("/user",verifyIfUserAlrearyExists,(request,response)=>{
 });
 
 
+app.post("/product",(request,response)=>{
+    const {name}=request.body;
+
+const products=product.some((products)=>products.name===name);
+
+if(products){
+    return response.status(400).json({message:"products already exists"})
+};
+
+product.push({
+name,
+id:uuidv4(),
+date: new Date(),
+Statement:[],
+});
+
+return response.status(201).json({message:"product create"})
+});
+
+app.get("/product",verifyIfProductAlrearyExists,(request,response)=>{
+    const {products}=request;
+
+    return response.status(201).json(products);
+})
